fa029e26f119bd7fad54ab2beb7580dd2fda7325
pakomercado0517 <sissyyayle0517@hotmail.com>
2026-02-14 17:43:00 -0600

    refactor: comment out TrustSection in HomePage component
    - Removed the TrustSection import and commented out its usage in the HomePage component to streamline the layout.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from 'react';
 import { Header } from './components/Header';
 import { HeroSection } from './components/HeroSection';
-import { TrustSection } from './components/TrustSection';
 import { FeaturesSection } from './components/FeaturesSection';
 import { DashboardPreview } from './components/DashboardPreview';
 import { PricingSection } from './components/PricingSection';
@@ -29,7 +28,7 @@ export default function HomePage() {
     <div className="bg-background min-h-screen">
       <Header />
       <HeroSection />
-      <TrustSection />
+      {/* <TrustSection /> */}
       <FeaturesSection />
       <DashboardPreview />
       <Suspense
